1dec5b6341740d7629404148c875c3db721cec77
TakahashiNg <83152264+TakahashiNguyen@users.noreply.github.com>
2026-03-25 03:18:02 +0000

    chore: update repository url

--- release.config.ts
+++ release.config.ts
@@ -2,7 +2,7 @@ import type { GlobalConfig } from "semantic-release";
 
 const config: GlobalConfig = {
   tagFormat: "v${version}",
-  repositoryUrl: "https://github.com/524H0003/TDTU-auto-login",
+  repositoryUrl: "https://git.demonkernel.io.vn/FoodSurf/frontend.git",
   branches: ["main", { name: "dev-*", prerelease: "dev" }],
   plugins: [
     "@semantic-release/commit-analyzer",
